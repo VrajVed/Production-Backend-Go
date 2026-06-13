be3e4c20fda2c04049f115310e6f75f595a1a5d4
VrajVed <vraj.a.ved@gmail.com>
2026-06-13 16:03:16 +0530

    mainconfig and taskfile

---
+++ backend/internal/config/config.go
@@ -0,0 +1,84 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/v2"
+	"github.com/rs/zerolog"
+)
+
+type Config struct {
+	Primary  Primary        `koanf:"primary" validate:"required"`
+	Server   ServerConfig   `koanf:"server" validate:"required"`
+	Database DatabaseConfig `koanf:"database" validate:"required"`
+	Auth     AuthConfig     `koanf:"auth" validate:"required"`
+	Redis    RedisConfig    `koanf:"redis" validate:"required"`
+}
+
+type Primary struct {
+	Env string `koanf:"env" validate:"required"`
+}
+
+type ServerConfig struct {
+	Port               string   `koanf:"port" validate:"required"`
+	ReadTimeout        int      `koanf:"read_timeout" validate:"required"`
+	WriteTimeout       int      `koanf:"write_timeout" validate:"required"`
+	IdleTimeout        int      `koanf:"idle_timeout" validate:"required"`
+	CORSAllowedOrigins []string `koanf:"cors_allowed_origins" validate:"required"`
+}
+
+type DatabaseConfig struct {
+	Host            string `koanf:"host" validate:"required"`
+	Port            int    `koanf:"port" validate:"required"`
+	User            string `koanf:"user" validate:"required"`
+	Password        string `koanf:"password" validate:"required"`
+	Name            string `koanf:"name" validate:"required"`
+	SSLMode         string `koanf:"ssl_mode" validate:"required"`
+	MaxOpenConns    int    `koanf:"max_open_conns" validate:"required"`
+	MaxIdleConns    int    `koanf:"max_idle_conns" validate:"required"`
+	ConnMaxLifetime int    `koanf:"conn_max_lifetime" validate:"required"`
+	ConnMaxIdleTime int    `koanf:"conn_max_idle_time" validate:"required"`
+}
+
+type RedisConfig struct {
+	Address string `koanf:"address" validate:"required"`
+}
+type AuthConfig struct {
+	SecretKey string `koanf:"secret_key" validate:"required"`
+}
+
+func LoadConfig() (*Config, error) {
+	// return a pointer cuz u want to read all variables and pass it around
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
+
+	k := koanf.New(".")
+
+	err := k.Load(env.Provider("BOILERPLATE_", ".", func(s string) string {
+		return strings.ToLower(strings.TrimPrefix(s, "BOILERPLATE_"))
+	}))
+
+	if err != nil {
+		logger.Fatal().Err(err).Msg("failed to load config from environment variables")
+	}
+
+	// now we marshal
+
+	mainConfig := &Config{}
+
+	err = k.Unmarshal("", mainConfig)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("failed to unmarshal config")
+	}
+
+	validate := validator.New(mainConfig)
+
+	err = validate.Struct(mainConfig)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("failed to validate config")
+	}
+
+	return mainConfig, nil
+}
